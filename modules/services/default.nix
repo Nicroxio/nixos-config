d2e082e1eed8a935a0868218b2213458c074a84e
Nicroxio <Ferdi@nicroxio.co.uk>
2025-09-04 20:06:06 +0100

    edit

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,14 +1,19 @@
-{config, pkgs, lib,  ...}:{
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
   imports = [
-  ./xserver.nix
-  ./kde.nix
-  ./cups.nix
-  ./pipewire.nix
-  ./ssh.nix
-  ./syncthing.nix
-  ]; 
+    ./xserver.nix
+    ./kde.nix
+    ./cups.nix
+    ./pipewire.nix
+    ./ssh.nix
+    ./syncthing.nix
+  ];
 
-  ssh.enable = 
-    lib.mkDefault  false;
+  ssh.enable = lib.mkDefault false;
 
 }
